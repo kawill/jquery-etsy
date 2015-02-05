217f50d6be2e948cb67e87f4ca062fb15d4e1fb9
Kristen <kawilliams3@gmail.com>
2015-02-05 08:36:16 -0600

    detailandsearchadded

--- js/etsy.js
+++ js/etsy.js
@@ -8,11 +8,18 @@
         var self = this;
         var EtsyRouter = Backbone.Router.extend({
             routes: {
-                "listings": "drawListingInfo"
+                "listing/:id": "drawDetails",
+                "search/:keyword": "drawSearch",
+                "*default": "drawListingInfo"
             },
             drawListingInfo: function() {
-
                 self.drawListing()
+            },
+            drawDetails: function(id){
+                self.drawDetailsInfo(id);
+            },
+            drawSearch: function(keyword) {
+                self.drawSearchInfo(keyword);
             },
             initialize: function() {
                 Backbone.history.start();
@@ -20,19 +27,29 @@
         })
         var router = new EtsyRouter();
 
-        this.drawListing();
+        // this.drawListing();
     }
 
     EtsyListing.prototype = {
         URLs: {
             listings: function() {
-                return "https://openapi.etsy.com/v2/listings/active.js?includes=Images:1"
+                return "https://openapi.etsy.com/v2/listings/active.js?includes=Images:1";
             },
-
+            detail: function(id){
+                return "https://openapi.etsy.com/v2/listings/"+id+".js?includes=Images:1";
+            },
+            search: function(){
+                return "https://openapi.etsy.com/v2/listings/active.js?includes=Images:1";
+            }
+            //keywordURL = getDataURL + "&keywords=" + this.keywords
 
         },
         access_token: function() {
-            return "&api_key=" + this.api_key + "&callback=?"
+            return "&api_key=" + this.api_key + "&callback=?";
+        },
+
+        search_extra: function(keyword) {
+            return "&keywords=" + keyword;
         },
 
         loadTemplate: function(template_name) {
@@ -44,29 +61,67 @@
 
 
         getListingData: function() {
-            return $.getJSON(this.URLs.listings() + this.access_token()).then(function(d, s, p) {
-                // console.log(d);
-                return d;
+            return $.getJSON(this.URLs.listings() + this.access_token()).then(function(d, s, p) {return d;});
+        },
+
+        getDetailData: function(id){
+            return $.getJSON(this.URLs.detail(id) + this.access_token()).then(function(d, s, p) {return d;});
+        },
+
+        getSearchData: function(keyword){
+            // return $.getJSON(this.URLs.search() + this.access_token() + this.search_extra(keyword)).then(function(d, s, p) {return d;});
+
+            var url =   this.URLs.search() +
+                        this.access_token() +
+                        this.search_extra(keyword)
+
+            console.log(url)
+
+            var promise = $.getJSON(url)
+
+
+            promise.then(function(d, s, p) {
+                 return d;
+
             });
+
+
+
+            return promise;
+
+
         },
 
 
         drawListing: function() {
             $.when(
                 this.getListingData(),
-                this.loadTemplate("listing")
-            ).then(function(listing, html) {
-                // console.log(listing)
-                var output1 = " ";
+                this.loadTemplate("listing") //"listing" = name of template file
+            ).then(function(listings, html) { //receive data & html from template
+                var compiledFunction = _.template(html);
+                $('.container').html( compiledFunction(listings) );
+            })
+        },
 
-                //     console.log(html)
-                var compiledFunction = _.template(html)
-                listing.results.forEach(function(result) {
-                    var output2 = compiledFunction(result);
-                    output1 += output2;
-                })
+        drawDetailsInfo: function(id){
+            $.when(
+                this.getDetailData(id),
+                this.loadTemplate("detail") //"listing" = name of template file
+            ).then(function(details, html) { //receive data & html from template
+                var compiledFunction = _.template(html);
+                $('.container').html( compiledFunction (details) );
+            })
+        },
 
-                $('.etsy-grid').append(output1);
+        drawSearchInfo: function(keyword) {
+            $.when(
+                this.getSearchData(keyword),
+                this.loadTemplate("search") //"listing" = name of template file
+            ).then(function(listings, html) { //receive data & html from template
+
+                var compiledFunction = _.template(html);
+
+                $('.container').html( compiledFunction (listings[0]) );
 
 
             })
@@ -80,6 +135,7 @@
     // var result = compiledFunction({hello: 'jkjlj'}); //pass object to function and function returns a string
 
     // console.log(result)
+    // $('body').on('click', '.listing', callback)
 
     window.EtsyListing = EtsyListing;
 
